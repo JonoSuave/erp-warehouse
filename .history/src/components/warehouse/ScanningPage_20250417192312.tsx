271dffc2b68ca6baa06d373f2fbd300dd3c38bd2
Jonathan Duncan <jonosuave@gmail.com>
2025-04-17 19:41:09 -0600

    feat: add ScanningPage component with ScanningInterface integration

---
+++ .history/src/components/warehouse/ScanningPage_20250417192312.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ScanningInterface from "./ScanningInterface";
+
+const ScanningPage: React.FC = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <ScanningInterface />
+    </div>
+  );
+};
+
+export default ScanningPage;
